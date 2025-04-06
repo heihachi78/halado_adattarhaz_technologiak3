511a5080bd487521616b5a9dd5f739e5cf78c742
Istvan Toth <tott.isti@gmail.com>
2025-04-06 13:59:58 +0200

    bug fixes

--- meltano/pemik-dwh/transform/models/facts/fact_booked_transactions.sql
+++ meltano/pemik-dwh/transform/models/facts/fact_booked_transactions.sql
@@ -8,7 +8,9 @@
 	  {'columns': ['purchase_id']},
 	  {'columns': ['case_id']},
 	  {'columns': ['debt_id']},
-	  {'columns': ['invoice_date']}
+	  {'columns': ['invoice_date']},
+	  {'columns': ['person_id']},
+	  {'columns': ['payment_id']}
     ]
 )}}
 
@@ -19,6 +21,8 @@ select
 	p.purchase_id,
 	c.case_id,
 	d.debt_id,
+	n.person_id,
+	m.payment_id,
 	d.calculated_to as invoice_date,
 	d.calculated_to - d.calculated_from as days_covered,
 	pd.debt_amount_covered,
@@ -31,16 +35,22 @@ from
 	{{ source("staging", "purchases") }} p,
 	{{ source("staging", "cases") }} c,
 	{{ source("staging", "debts") }} d,
-	{{ source("staging", "payed_debts") }} pd
+	{{ source("staging", "payed_debts") }} pd,
+	{{ source("staging", "payments") }} m,
+	{{ source("staging", "persons") }} n
 where
 	s.sector_id = r.sector_id and
 	r.partner_id = p.partner_id and
 	p.purchase_id = c.purchase_id and
 	c.case_id = d.case_id and
 	d.debt_id = pd.debt_id and
+	n.person_id = m.person_id and
+	m.payment_id = pd.payment_id and
 	s.deleted_at is null and
 	r.deleted_at is null and
 	p.deleted_at is null and
 	c.deleted_at is null and
 	d.deleted_at is null and
-	pd.deleted_at is null
+	pd.deleted_at is null and
+	n.deleted_at is null and
+	m.deleted_at is null
