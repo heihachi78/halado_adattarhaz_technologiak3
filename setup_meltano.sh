47032b259ae92e8befb0e189b988b515c233faf2
Tóth István <tott.isti@gmail.com>
2025-04-06 19:44:50 +0200

    mac bugfix

--- setup_meltano.sh
+++ setup_meltano.sh
@@ -3,6 +3,7 @@
 docker exec prefsrv sh -c "cd /mnt/flows && hap run python meltano_flow.py"
 docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && meltano install"
 docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && meltano lock --update --all"
+docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && hap run meltano invoke dbt-postgres:deps"
 docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && hap run meltano run srv1-extract dwhdb-load --no-state-update --full-refresh"
 docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && hap run meltano invoke dbt-postgres:docs-generate"
 docker exec prefsrv sh -c "cd /mnt/meltano/pemik-dwh && hap run meltano invoke dbt-postgres:docs-serve --host 0.0.0.0 --port 8080"
